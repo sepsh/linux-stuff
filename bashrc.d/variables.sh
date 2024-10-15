cd0b9b4e436abf3b119e986bb80c6c0a3f258041
Sepehr Shirkhanlou <sepehr.shirkhanlou@yahoo.com>
2024-10-16 01:37:35 +0330

    Set EDITOR

--- bashrc.d/variables.sh
+++ bashrc.d/variables.sh
@@ -1,1 +1,11 @@
 export UV_PYTHON_PREFERENCE=only-managed
+
+if [[ $(type -t vim) == "file" ]]; then
+	export EDITOR="$(which vim)"
+elif [[ $(type -t vim) == "alias" ]]; then
+	export EDITOR="$(which --skip-alias vim)"
+elif [[ $(type -t nano) == "file" ]]; then
+	export EDITOR="$(which nano)"
+elif [[ $(type -t nano) == "alias" ]]; then
+	export EDITOR="$(which --skip-alias nano)"
+fi
