8bc0fe11c764d8e99c192e4fef950d680232849e
Sepehr Shirkhanlou <sepehr.shirkhanlou@yahoo.com>
2024-10-15 19:08:44 +0330

    Add aliases for ls

--- bashrc.d/aliases.sh
+++ bashrc.d/aliases.sh
@@ -3,6 +3,20 @@
 declare -A my_aliases
 
 
+# Aliases for "ls"
+
+if [[ $(type -t ls) == "alias" ]]; then
+	ls="${BASH_ALIASES[ls]}"
+else
+	ls="ls"
+fi
+my_aliases[ll]="$ls -l"
+my_aliases[la]="$ls -a"
+my_aliases[lla]="$ls -la"
+my_aliases[lal]="$ls -al"
+unset ls
+
+
 # All aliases will be set in this loop
 
 for key in "${!my_aliases[@]}"; do
