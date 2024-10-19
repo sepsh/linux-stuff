21683e68801d92ce39c7badd325d6c6e9195ec4f
Sepehr Shirkhanlou <sepehr.shirkhanlou@yahoo.com>
2024-10-19 13:48:44 +0330

    Update `setup.sh` to add `.bashrc.d` in `.bashrc`

--- setup.sh
+++ setup.sh
@@ -13,3 +13,24 @@ else
 	ln -s $bashrcd_source $bashrcd_target
 	)
 fi
+
+# Configre "~/.bashrc" to load .sh files from "~/.bashrc.d/"
+
+if [[ ! $(cat ~/.bashrc | grep '.bashrc.d') ]]; then
+	echo Configuring '~/.bashrc' to include './.bashrd.d/*sh'
+	cat << EOF >> ~/.bashrc
+# Created automatically by https://github.com/sepsh/linux-stuff/blob/main/setup.sh
+
+if [ -d ~/.bashrc.d ]; then
+	for rc in ~/.bashrc.d/*.sh; do
+		if [ -f \$rc ]; then
+			. "\$rc"
+		fi
+	done
+fi
+unset rc
+
+EOF
+else
+	echo ~/.bashrc is configured. skipping
+fi
