e84415e4240ba92114cc3fbed5ff200e754a2599
Sepehr Shirkhanlou <sepehr.shirkhanlou@yahoo.com>
2024-10-27 19:24:05 +0330

    Reorder autocompletes

--- bashrc.d/autocompletions.sh
+++ bashrc.d/autocompletions.sh
@@ -1,4 +1,4 @@
+if [[ $(type -t pipx) == "file" ]]; then eval "$(register-python-argcomplete pipx)"; fi
+
 if [[ $(type -t uv) == "file" ]]; then eval "$(uv generate-shell-completion bash)"; fi
 if [[ $(type -t uvx) == "file" ]]; then eval "$(uvx --generate-shell-completion bash)"; fi
-
-if [[ $(type -t pipx) == "file" ]]; then eval "$(register-python-argcomplete pipx)"; fi
