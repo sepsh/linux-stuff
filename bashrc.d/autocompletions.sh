2b35fc7ca2dd34d6d42f70d9bc30b42e517934cc
Sepehr Shirkhanlou <sepehr.shirkhanlou@yahoo.com>
2024-11-04 15:52:19 +0330

    Add autocomplete for `kubectl`

--- bashrc.d/autocompletions.sh
+++ bashrc.d/autocompletions.sh
@@ -2,3 +2,6 @@ if [[ $(type -t pipx) == "file" ]]; then eval "$(register-python-argcomplete pip
 
 if [[ $(type -t uv) == "file" ]]; then eval "$(uv generate-shell-completion bash)"; fi
 if [[ $(type -t uvx) == "file" ]]; then eval "$(uvx --generate-shell-completion bash)"; fi
+
+if [[ $(type -t kubectl) == "file" ]]; then source <(kubectl completion bash); fi
+
