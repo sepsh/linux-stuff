fc28ba0db336910cdbbbd07fcab096a9c8eb3be2
Sepehr Shirkhanlou <sepehr.shirkhanlou@yahoo.com>
2024-11-23 06:02:19 +0330

    Add function command_exists

--- bashrc.d/autocompletions.sh
+++ bashrc.d/autocompletions.sh
@@ -1,7 +1,13 @@
-if [[ $(type -t pipx) == "file" ]]; then eval "$(register-python-argcomplete pipx)"; fi
+function command_exists() {
+	return $(command -v $1 > /dev/null)
+}
 
-if [[ $(type -t uv) == "file" ]]; then eval "$(uv generate-shell-completion bash)"; fi
-if [[ $(type -t uvx) == "file" ]]; then eval "$(uvx --generate-shell-completion bash)"; fi
+if command_exists pipx; then eval "$(register-python-argcomplete pipx)"; fi
 
-if [[ $(type -t kubectl) == "file" ]]; then source <(kubectl completion bash); fi
+if command_exists uv; then eval "$(uv generate-shell-completion bash)"; fi
+if command_exists uvx; then eval "$(uvx --generate-shell-completion bash)"; fi
+
+if command_exists kubectl; then source <(kubectl completion bash); fi
+
+unset command_exists
 
