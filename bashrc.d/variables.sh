41473b726b1b80a86b0aecd37cad1566fcbe5dca
Sepehr Shirkhanlou <sepehr.shirkhanlou@yahoo.com>
2024-11-23 06:17:25 +0330

    Update PATH

--- bashrc.d/variables.sh
+++ bashrc.d/variables.sh
@@ -9,3 +9,5 @@ elif [[ $(type -t nano) == "file" ]]; then
 elif [[ $(type -t nano) == "alias" ]]; then
 	export EDITOR="$(which --skip-alias nano)"
 fi
+
+for p in "$HOME/.local/bin" "/usr/local/sbin" "/sbin"; do if [[ $PATH != *"$p"* ]]; then export PATH="$PATH:$p"; fi; done; unset p
