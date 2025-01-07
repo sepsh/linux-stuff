25b8f1257fb3b07d661acd3fda82bb50d29d1375
Sepehr Shirkhanlou <sepehr.shirkhanlou@yahoo.com>
2025-01-07 19:43:46 +0330

    Add aliases for `tmux`

---
+++ bashrc.d/aliases.sh.d/tmux.sh
@@ -0,0 +1,14 @@
+# Aliases for "tmux"
+# this files will be sources by aliases.sh
+
+
+# Static aliases
+
+
+my_aliases[tml]="tmux ls"
+my_aliases[tmn]="tmux new -s"
+my_aliases[tma]="tmux attach -t"
+my_aliases[tmk]="tmux kill-session -t"
+
+
+# Dynamic aliases
