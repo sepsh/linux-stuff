271aafc9ee573239380c533a1e39372f2d4ccb89
Sepehr Shirkhanlou <sepehr.shirkhanlou@yahoo.com>
2025-01-09 13:50:07 +0330

    Add more meaningfull aliases

--- bashrc.d/aliases.sh.d/tmux.sh
+++ bashrc.d/aliases.sh.d/tmux.sh
@@ -6,9 +6,16 @@
 
 
 my_aliases[tml]="tmux ls"
+my_aliases[tmls]="tmux ls"
+
 my_aliases[tmn]="tmux new -s"
+my_aliases[tmnew]="tmux new -s"
+
 my_aliases[tma]="tmux attach -t"
+my_aliases[tmattach]="tmux attach -t"
+
 my_aliases[tmk]="tmux kill-session -t"
+my_aliases[tmkill]="tmux kill-session -t"
 
 
 # Dynamic aliases
