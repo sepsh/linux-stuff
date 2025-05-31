b7226446959b98eb3793dec5108fbd5d862fba63
Sepehr Shirkhanlou <sepehr.shirkhanlou@yahoo.com>
2025-05-31 10:51:12 +0330

    Add shell completions for Kind

--- bashrc.d/40-autocompletions.sh
+++ bashrc.d/40-autocompletions.sh
@@ -9,5 +9,7 @@ if command_exists uvx; then eval "$(uvx --generate-shell-completion bash)"; fi
 
 if command_exists kubectl; then source <(kubectl completion bash); fi
 
+if command_exists kind; then source <(kind completion bash); fi
+
 unset command_exists
 
