64953c33bfa2965a94f9d38e094920a536ec474f
Sepehr Shirkhanlou <sepehr.shirkhanlou@yahoo.com>
2025-06-03 15:56:43 +0330

    Add shell completions for ArgoCD CLI

--- bashrc.d/40-autocompletions.sh
+++ bashrc.d/40-autocompletions.sh
@@ -13,5 +13,7 @@ if command_exists kubectl; then source <(kubectl completion bash); fi
 
 if command_exists kind; then source <(kind completion bash); fi
 
+if command_exists kind; then source <(argocd completion bash); fi
+
 unset command_exists
 
