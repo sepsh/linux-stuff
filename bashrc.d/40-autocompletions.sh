02dd7b20b94b14c409f7bbce9c2c1de64b7dc261
Sepehr Shirkhanlou <sepehr.shirkhanlou@yahoo.com>
2025-08-24 15:10:47 +0330

    Fix `pipx` autocomplete

--- bashrc.d/40-autocompletions.sh
+++ bashrc.d/40-autocompletions.sh
@@ -2,7 +2,13 @@ function command_exists() {
 	return $(command -v $1 > /dev/null)
 }
 
-if command_exists pipx; then eval "$(register-python-argcomplete3 pipx)"; fi
+if command_exists pipx; then
+	if command_exists register-python-argcomplete; then
+		eval "$(register-python-argcomplete pipx)";
+	elif command_exists register-python-argcomplete3; then
+		eval "$(register-python-argcomplete3 pipx)";
+	fi
+fi
 
 if command_exists uv; then eval "$(uv generate-shell-completion bash)"; fi
 if command_exists uvx; then eval "$(uvx --generate-shell-completion bash)"; fi
